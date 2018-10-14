5a814b352cfb731dedc397af9cccacfcd50a6c40
simpart <simpart>
2018-10-14 05:00:48 +0000

    add comment

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
  * @author simpart
  */
 const mf = require('mofron');
-
 /**
  * @class Font
  * @brief font effect class
@@ -14,7 +13,7 @@ mf.effect.Font = class extends mf.Effect {
         try {
             super();
             this.name('Font');
-            this.prmMap('fontName', 'path');
+            this.prmMap(['fontName', 'path']);
             this.prmOpt(po, p2);
         } catch (e) {
             console.error(e.stack);
@@ -22,6 +21,13 @@ mf.effect.Font = class extends mf.Effect {
         }
     }
     
+    /**
+     * setter/getter font name
+     *
+     * @param p1 (string) primary font name
+     * @param p2 (string) secondary font name
+     * @return (string) font name
+     */
     fontName (p1, p2) {
         try {
             return this.member(
@@ -35,6 +41,13 @@ mf.effect.Font = class extends mf.Effect {
         }
     }
     
+    /**
+     * setter/getter path to font
+     * 
+     * @param prm (string) path to font
+     * @param prm (undefined) call as getter
+     * @return path to font
+     */
     path (prm) {
         try { return this.member('path', 'string', prm); } catch (e) {
             console.error(e.stack);
@@ -42,6 +55,11 @@ mf.effect.Font = class extends mf.Effect {
         }
     }
     
+    /**
+     * enable text font
+     *
+     * @note private method
+     */
     enable (cmp) {
         try {
             mofron.func.setFontFace(this.fontName(), this.path());
@@ -52,6 +70,11 @@ mf.effect.Font = class extends mf.Effect {
         }
     }
     
+    /**
+     * disable text font
+     *
+     * @note private method
+     */
     disable (cmp) {
         try { cmp.style({ 'font-family' : null }); } catch (e) {
             console.error(e.stack);
